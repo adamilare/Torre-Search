e80f4f9840c9cff6227bcdd07f8dbc4c65a5e48b
Damilare Adepoju <108327427+adamilare@users.noreply.github.com>
2023-09-13 15:38:00 +0100

    Update environment

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
 export const BASE_URL =
-  process.env.REACT_APP_BASE_URL || `https://search-test-server.onrender.com`;
+  process.env.REACT_APP_BASE_URL || `https://search-test-server.onrender.com/`;
